174c215a185ef45d7e077da58d542d7e12c087a1
Sandro Jukic <inf4078@hs-worms.de>
2025-07-04 19:25:27 +0200

    main app routing

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'admin', loadChildren: () => import('./admin/admin-module').then(m => m.AdminModule) }, { path: 'user', loadChildren: () => import('./user/user-module').then(m => m.UserModule) }, { path: 'shop', loadChildren: () => import('./shop/shop-module').then(m => m.ShopModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'shop', pathMatch: 'full' },
+  { path: 'admin', loadChildren: () => import('./admin/admin-module').then(m => m.AdminModule) },
+  { path: 'user', loadChildren: () => import('./user/user-module').then(m => m.UserModule) },
+  { path: 'shop', loadChildren: () => import('./shop/shop-module').then(m => m.ShopModule) },
+  { path: '**', redirectTo: 'shop' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
