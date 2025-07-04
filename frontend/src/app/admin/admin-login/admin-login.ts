263df562e74467c53c6e951cb5298bcb0691d5bc
Sandro Jukic <inf4078@hs-worms.de>
2025-07-04 21:27:34 +0200

    implemented route based redirection for admin as well

--- frontend/src/app/admin/admin-login/admin-login.ts
+++ frontend/src/app/admin/admin-login/admin-login.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { AdminAuthService } from '../../core/admin-auth';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-admin-login',
   standalone: false,
   templateUrl: './admin-login.html',
   styleUrl: './admin-login.scss'
 })
 export class AdminLogin {
+  adminName = '';
+  adminPassword = '';
+  returnUrl = '/admin/dashboard';
 
+  constructor(
+    private adminAuthService: AdminAuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe(params => {
+      if (params['returnUrl']) {
+        this.returnUrl = params['returnUrl'];
+      }
+    });
+  }
+
+  login() {
+    this.adminAuthService.login({ adminName: this.adminName, adminPassword: this.adminPassword }).subscribe(() => {
+      this.router.navigateByUrl(this.returnUrl);
+    });
+  }
 }
