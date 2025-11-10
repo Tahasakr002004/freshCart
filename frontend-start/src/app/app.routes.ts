4aa49e249918938eec47a8e30453bf10a3c986d0
Mari <inf3081>
2025-11-10 20:13:32 +0100

    a

--- frontend-start/src/app/app.routes.ts
+++ frontend-start/src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { authGuard } from './guards/auth.guard';
 import { Dashboard } from './dashboard/dashboard';
 import { SignIn } from './sign-in/sign-in';
 import { SignUp } from './sign-up/sign-up';
-import { Shop } from './shop/shop';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
   { path: 'sign-in', component: SignIn },
   { path: 'sign-up', component: SignUp },
-  { path: 'shop', component: Shop },
   { path: '**', redirectTo: 'dashboard' }
 ];
